8b24a71ee27e4b6e9a09bd8a4981abf474f56e7c
Erich Eichinger <eeichinger@users.noreply.github.com>
2023-08-26 08:09:54 +0200

    initial project from spring initializr

---
+++ src/main/java/org/github/eeichinger/examples/httpcachecontroldemo/HttpClientCacheControlDemoApplication.java
@@ -0,0 +1,13 @@
+package org.github.eeichinger.examples.httpcachecontroldemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HttpClientCacheControlDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HttpClientCacheControlDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/github/eeichinger/examples/httpcachecontroldemo/HttpClientCacheControlDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package org.github.eeichinger.examples.httpcachecontroldemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HttpClientCacheControlDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
